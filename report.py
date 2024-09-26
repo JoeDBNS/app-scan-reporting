a6b64d86798b3b4eadcb12116c893ba665c86947
Joseph Davis <davisj38@michigan.gov>
2024-09-26 13:37:32 -0400

    Move project ID value to config.json

--- report.py
+++ report.py
@@ -5,19 +5,20 @@ import requests
 
 try:
     with open('./config/main.json') as file:
-      config = json.load(file)
+        config = json.load(file)
 except:
     config = False
-    print("ERROR: File not found (./config/main.json)")
+    print('ERROR: File not found (./config/main.json)')
 
 
 
 if (config != False):
     try:
-        request = requests.get("https://asoc.state.mi.us/srm/api/projects/1037/findings/count", headers={"Content-Type":"application/json", "API-Key":config['secret-token']})
+        request_url = 'https://asoc.state.mi.us/srm/api/projects/' + config['project-id'] + '/findings/count'
+        request = requests.get(request_url, headers={'Content-Type':'application/json', 'API-Key':config['secret-token']})
     except:
         request = False
-        print("ERROR: Must be on SOM network")
+        print('ERROR: Must be on SOM network')
 
 
     print(request.text)
