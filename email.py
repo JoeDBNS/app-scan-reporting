8556da2004ed9b5d8cdbb04f7feb28ac96639a00
Joseph Davis <davisj38@michigan.gov>
2024-10-07 14:06:36 -0400

    Build initial email module

---
+++ email.py
@@ -0,0 +1,33 @@
+import json, smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.utils import formataddr
+
+
+# Load config json from local config.json file
+def LoadConfigFile():
+    try:
+        with open('./config/main.json') as file:
+            return json.load(file)
+    except:
+        print('ERROR - LoadConfigFile()')
+        return False
+
+
+def SendEmailWithAttachment(recipient, subject, body, attachment_path, attachment_name):
+    config = LoadConfigFile()
+
+    message = MIMEMultipart()
+    message['Subject'] = subject
+    message['To'] = recipient
+    message['From'] = formataddr((config['smtp']['sender']["name"], config['smtp']['sender']["email"]))
+
+    body_part = MIMEText(body)
+    message.attach(body_part)
+
+    with open(attachment_path, 'rb') as file:
+        message.attach(MIMEApplication(file.read(), Name=attachment_name))
+
+    with smtplib.SMTP(config['smtp']['host'], config['smtp']['port']) as server:
+        server.send_message(message)
