6c9afe78d14a0877479948ee988fb8d2b49b59f0
Joseph Davis <joedbns@gmail.com>
2024-11-08 22:03:28 -0500

    Build initial main.json file if not found

--- setup_tool.ps1
+++ setup_tool.ps1
@@ -1,1 +1,35 @@
 pip install openpyxl
+
+$config_text = @'
+{
+    "smtp": {
+        "host": "",
+        "port": 0,
+        "sender": {
+            "name": "",
+            "email": ""
+        }
+    },
+    "hosts": {
+        "srm": ""
+    },
+    "secret-token": "",
+    "projects": [
+        {
+            "id": 0,
+            "name": "",
+            "contacts": [
+                {
+                    "role": "",
+                    "name": "",
+                    "email": ""
+                }
+            ]
+        }
+    ]
+}
+'@
+
+if (!(Test-Path './src/config/main.json')) {
+    New-Item './src/config/main.json' -value $config_text
+}
