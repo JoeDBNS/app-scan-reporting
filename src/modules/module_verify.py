6caa05ba7875e0120bbaaf52566d533cafffc609
Joseph Davis <davisj38@michigan.gov>
2024-11-15 10:51:44 -0500

    Fix issue where verification check was overwriting config data

--- src/modules/module_verify.py
+++ src/modules/module_verify.py
@@ -1,4 +1,4 @@
-import json, subprocess
+import json, subprocess, copy
 import modules.module_console as con
 
 
@@ -20,85 +20,92 @@ def PrepareReport(report):
 
 
 def VerifyConfigFile(config):
+    config_report = copy.deepcopy(config)
     issues_found = 0
 
     try:
-        if (config != False):
-            if (config['smtp']['host'] != ''):
-                config['smtp']['host'] = '__GOOD__'
+        if (config_report != False):
+            if (config_report['smtp']['host'] != ''):
+                config_report['smtp']['host'] = '__GOOD__'
             else:
                 issues_found += 1
-                config['smtp']['host'] = '__BAD__'
+                config_report['smtp']['host'] = '__BAD__'
 
-            if (config['smtp']['port'] != 0):
-                config['smtp']['port'] = '__GOOD__'
+            if (config_report['smtp']['port'] != 0):
+                config_report['smtp']['port'] = '__GOOD__'
             else:
                 issues_found += 1
-                config['smtp']['port'] = '__BAD__'
+                config_report['smtp']['port'] = '__BAD__'
 
-            if (config['smtp']['sender']['name'] != ''):
-                config['smtp']['sender']['name'] = '__GOOD__'
+            if (config_report['smtp']['sender']['name'] != ''):
+                config_report['smtp']['sender']['name'] = '__GOOD__'
             else:
                 issues_found += 1
-                config['smtp']['sender']['name'] = '__BAD__'
+                config_report['smtp']['sender']['name'] = '__BAD__'
 
-            if (config['smtp']['sender']['email'] != ''):
-                config['smtp']['sender']['email'] = '__GOOD__'
+            if (config_report['smtp']['sender']['email'] != ''):
+                config_report['smtp']['sender']['email'] = '__GOOD__'
             else:
                 issues_found += 1
-                config['smtp']['sender']['email'] = '__BAD__'
+                config_report['smtp']['sender']['email'] = '__BAD__'
 
-            if (config['hosts']['srm'] != ''):
-                config['hosts']['srm'] = '__GOOD__'
+            if (config_report['hosts']['srm'] != ''):
+                config_report['hosts']['srm'] = '__GOOD__'
             else:
                 issues_found += 1
-                config['hosts']['srm'] = '__BAD__'
+                config_report['hosts']['srm'] = '__BAD__'
 
-            if (config['secret-token'] != ''):
-                config['secret-token'] = '__GOOD__'
+            if (config_report['hosts']['internal-network'] != ''):
+                config_report['hosts']['internal-network'] = '__GOOD__'
             else:
                 issues_found += 1
-                config['secret-token'] = '__BAD__'
+                config_report['hosts']['internal-network'] = '__BAD__'
 
-            if (len(config['projects']) == 0):
-                issues_found += 1
-                config['projects'] = '__BAD__'
+            if (config_report['secret-token'] != ''):
+                config_report['secret-token'] = '__GOOD__'
             else:
-                for i_proj, project in enumerate(config['projects']):
+                issues_found += 1
+                config_report['secret-token'] = '__BAD__'
+
+            if (len(config_report['projects']) == 0):
+                issues_found += 1
+                config_report['projects'] = '__BAD__'
+            else:
+                for i_proj, project in enumerate(config_report['projects']):
                     if (project['id'] != ''):
-                        config['projects'][i_proj]['id'] = '__GOOD__'
+                        config_report['projects'][i_proj]['id'] = '__GOOD__'
                     else:
                         issues_found += 1
-                        config['projects'][i_proj]['id'] = '__BAD__'
+                        config_report['projects'][i_proj]['id'] = '__BAD__'
 
                     if (project['name'] != ''):
-                        config['projects'][i_proj]['name'] = '__GOOD__'
+                        config_report['projects'][i_proj]['name'] = '__GOOD__'
                     else:
                         issues_found += 1
-                        config['projects'][i_proj]['name'] = '__BAD__'
+                        config_report['projects'][i_proj]['name'] = '__BAD__'
 
                     if (len(project['contacts']) == 0):
                         issues_found += 1
-                        config['projects'][i_proj]['contacts'] = '__BAD__'
+                        config_report['projects'][i_proj]['contacts'] = '__BAD__'
                     else:
                         for i_cont, contact in enumerate(project['contacts']):
                             if (contact['role'] != ''):
-                                config['projects'][i_proj]['contacts'][i_cont]['role'] = '__GOOD__'
+                                config_report['projects'][i_proj]['contacts'][i_cont]['role'] = '__GOOD__'
                             else:
                                 issues_found += 1
-                                config['projects'][i_proj]['contacts'][i_cont]['role'] = '__BAD__'
+                                config_report['projects'][i_proj]['contacts'][i_cont]['role'] = '__BAD__'
 
                             if (contact['name'] != ''):
-                                config['projects'][i_proj]['contacts'][i_cont]['name'] = '__GOOD__'
+                                config_report['projects'][i_proj]['contacts'][i_cont]['name'] = '__GOOD__'
                             else:
                                 issues_found += 1
-                                config['projects'][i_proj]['contacts'][i_cont]['name'] = '__BAD__'
+                                config_report['projects'][i_proj]['contacts'][i_cont]['name'] = '__BAD__'
 
                             if (contact['email'] != ''):
-                                config['projects'][i_proj]['contacts'][i_cont]['email'] = '__GOOD__'
+                                config_report['projects'][i_proj]['contacts'][i_cont]['email'] = '__GOOD__'
                             else:
                                 issues_found += 1
-                                config['projects'][i_proj]['contacts'][i_cont]['email'] = '__BAD__'
+                                config_report['projects'][i_proj]['contacts'][i_cont]['email'] = '__BAD__'
 
 
         else:
@@ -107,10 +114,10 @@ def VerifyConfigFile(config):
 
 
         if (issues_found != 0):
-            print('\n', PrepareReport(json.dumps(config, indent=4)))
+            print('\n', PrepareReport(json.dumps(config_report, indent=4)))
 
         else:
-            print('\n', PrepareReport(json.dumps(config, indent=4)))
+            print('\n', PrepareReport(json.dumps(config_report, indent=4)))
             con.Pass('\nConfig File Was Verified and Passed')
 
     except Exception as error:
