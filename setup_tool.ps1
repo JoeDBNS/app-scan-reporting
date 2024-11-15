d22fc0f7bc74528e55a5bfa572b2ab4990543f61
Joseph Davis <davisj38@michigan.gov>
2024-11-15 10:50:27 -0500

    Add user feedback for main.json file action

--- setup_tool.ps1
+++ setup_tool.ps1
@@ -33,4 +33,10 @@ $config_text = @'
 
 if (!(Test-Path './src/config/main.json')) {
     New-Item './src/config/main.json' -value $config_text
+    Write-Host "`n"
+    Write-Host "'main.json' File Created" -ForegroundColor "Green"
+}
+else {
+    Write-Host "`n"
+    Write-Host "'main.json' File Already Exists" -ForegroundColor "Green"
 }
