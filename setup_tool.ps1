fa487e10bacf2aa5c3617a3d11046261033ee7cf
Joseph Davis <davisj38@michigan.gov>
2024-11-15 14:13:31 -0500

    Add more steps to setup_tool.ps1

--- setup_tool.ps1
+++ setup_tool.ps1
@@ -1,4 +1,13 @@
+choco -v
+
+# @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install -y python3
+
+python --version
+
 pip install openpyxl
+pip install requests
 
 $config_text = @'
 {
