5978d010fb5e1bfe6f06782403eff33a73cb4930
Joseph Davis <davisj38@michigan.gov>
2025-01-16 16:02:11 -0500

    Progress on config build

--- src/run_admin.py
+++ src/run_admin.py
@@ -59,9 +59,40 @@ if (config != False):
 
     elif (action_choice == 'build'):
         print()
-        email_sender_name = input('Email Display Name: ').strip()
-        email_sender_email = input('Email Address: ').strip()
-        secret_token = input('SRM Secret Token: ').strip()
+        email_sender_name = input('Email Display Name:\t').strip()
+        email_sender_email = input('Email Address:\t\t').strip()
+        secret_token = input('SRM Secret Token:\t').strip()
+
+        projects_choice = input('\nDo you want to add a project? (y/n) ').lower().strip()
+        projects = []
+
+        while (projects_choice == 'y'):
+            new_project = {}
+            new_project_id = input('Project Id: ').strip()
+            new_project_name = input('Project Name: ').strip()
+
+            contacts_choice = 'y'
+            contacts = []
+
+            while (contacts_choice == 'y'):
+                new_contact = {}
+                new_contact_role = input('Contact Role: ').strip()
+                new_contact_name = input('Contact Name: ').strip()
+
+                emails_choice = 'y'
+                emails = []
+
+                while (emails_choice == 'y'):
+                    new_email = input('Email Address: ').strip()
+
+                    emails.append(new_email)
+                    emails_choice = input('\nDo you want to add another email? (y/n) ').lower().strip()
+
+                contacts.append(new_contact)
+                contacts_choice = input('\nDo you want to add another contact? (y/n) ').lower().strip()
+
+            projects.append(new_project)
+            projects_choice = input('\nDo you want to add another project? (y/n) ').lower().strip()
 
     elif (action_choice == 'edit'):
         print()
