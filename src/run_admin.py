daa338608760664086e1c65617cadb1e00824220
Joseph Davis <davisj38@michigan.gov>
2025-01-16 15:28:54 -0500

    Build out initial view in run_admin

--- src/run_admin.py
+++ src/run_admin.py
@@ -22,5 +22,43 @@ config = LoadConfigFile()
 if (config != False):
     con.Pass('Config File Loaded')
 
+    action_options = ['view', 'edit']
+    action_choice = input('\nDo you want to run [view] or [edit] your config file? ').lower().strip()
+
+    while (action_choice not in action_options):
+        print('invalid entry')
+        action_choice = input('\n\nDo you want to run [view] or [edit] your config file? ').lower().strip()
+
+    if (action_choice == 'view'):
+        print('\nsmtp:', json.dumps(json.loads(str(config['smtp']).replace('\'', '"')), indent=4))
+        print('\nhosts:', json.dumps(json.loads(str(config['hosts']).replace('\'', '"')), indent=4))
+        print('\nsecret-token:', '"' + config['secret-token'] + '"')
+        print('\nprojects:')
+
+        for project in config['projects']:
+            print()
+            print('\tid:', '"' + str(project['id']) + '"')
+            print('\tname:', '"' + project['name'] + '"')
+            print('\treports:')
+
+            for report in project['reports']:
+                print('\t\ttype:', '"' + report['type'] + '"')
+                print('\t\tcontacts:')
+
+                for contact in report['contacts']:
+                    print('\t\t\trole:', '"' + contact['role'] + '"')
+                    print('\t\t\tname:', '"' + contact['name'] + '"')
+                    print('\t\t\temails:')
+
+                    for email in contact['emails']:
+                        print('\t\t\t\t' + '"' + email + '"')
+
+                    print()
+
+                print()
+
+    elif (action_choice == 'edit'):
+        print('edit')
+
 else:
     con.Error('ERROR:\tLOADING CONFIG FILE')
